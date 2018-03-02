672a0cd97f2fead23d8c38e0bed47e738585a968
Marcos Benatti Lauer <benatti77@gmail.com>
2018-03-02 04:23:59 -0300

    prepare for docs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule CoinCap.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :coincap_ex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
+      package: package(),
+      description: description(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Docs
+      name: "CoinCap.io API Client",
+      source_url: "https://github.com/mbenatti/coincap_ex",
+      homepage_url: "https://github.com/mbenatti/coincap_ex/README.md"
     ]
   end
 
@@ -20,7 +28,21 @@ defmodule CoinCap.MixProject do
   defp deps do
     [
       {:poison, "~> 3.0"},
-      {:tesla, "~> 0.10.0"}
+      {:tesla, "~> 0.10.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    [maintainers: ["Marcos Benatti Lauer"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mbenatti/coincap_ex"},
+     files: ~w(mix.exs README.md lib)]
+  end
+
+  defp description do
+    """
+    CoinCap.io API Client
+    """
   end
 end

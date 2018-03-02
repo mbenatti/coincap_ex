9d6943df3d6effca2f60d1f484940c0c58c98b9b
Marcos Benatti Lauer <benatti77@gmail.com>
2018-03-02 03:20:47 -0300

    mix files

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule CoinCap.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :coincap_ex,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:jason, "~> 1.0"},
+      {:tesla, "~> 0.10.0"}
+    ]
+  end
+end
